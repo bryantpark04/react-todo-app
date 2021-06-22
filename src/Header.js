736d1f9201395740dbfa6f82ef20b867b8ac23c6
Bryant Park <52787152+bryantpark04@users.noreply.github.com>
2021-06-22 02:49:42 -0400

    Make Header text a tiny bit bigger

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,7 @@ import Col from 'react-bootstrap/Col';
 
 const Header = () => {
     return (
-        <Col className="Header">
-            <h1><strong>Todo App</strong></h1>
-        </Col>
+        <h1 className="Header"><strong>Todo App</strong></h1>
     )
 }
 
